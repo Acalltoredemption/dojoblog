ab148fd9f8694a9970d22f27720f872395d89b9d
Acalltoredemption <acalltoredemption@gmail.com>
2021-01-12 07:16:25 -0800

    some initial styling for blog component

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,21 @@ import React, {useState} from 'react';
 
 
 const  Home = () => {
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
 
-    const handleClick = () => {
-      setName('luigi');
-      setAge(30);
-    }
-
+const [blogs, setBlogs] = useState([
+    {title: 'My new website', body: 'blah blah blah', author: 'mario', id: 1},
+    {title: 'My better website', body: 'blah blah blah', author: 'mario', id: 2},
+    {title: 'Welcome Party!', body: 'blah blah blah', author: 'yoshi', id: 3}
+])
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                </div>
+            ))}
 
         </div>
      );
